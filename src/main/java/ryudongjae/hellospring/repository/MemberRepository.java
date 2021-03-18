129ad2659ffcf1f0f064651b1268831627a2deda
ryudongjae <rdj10149@gmail.com>
2021-03-18 19:07:22 +0900

    Commit

--- src/main/java/ryudongjae/hellospring/repository/MemberRepository.java
+++ src/main/java/ryudongjae/hellospring/repository/MemberRepository.java
@@ -4,6 +4,7 @@ import ryudongjae.hellospring.domain.Member;
 import java.util.List;
 import java.util.Optional;
 
+
 public interface MemberRepository {
     Member save(Member member);
     Optional<Member> findById(Long id);
