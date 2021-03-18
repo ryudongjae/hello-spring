8dec849dd270d39e022db742ef371355f5fef195
ryudongjae <rdj10149@gmail.com>
2021-03-19 02:13:09 +0900

    Update

--- src/main/java/ryudongjae/hellospring/service/MemberService.java
+++ src/main/java/ryudongjae/hellospring/service/MemberService.java
@@ -30,7 +30,7 @@ public class MemberService {
         memberRepository.findByName(member.getName())
                 .ifPresent(m -> {
                     throw new IllegalStateException("이미 존재하는 회원입니다.");
-                  });
+                });
     }
     /*
     전체 회원 조회
