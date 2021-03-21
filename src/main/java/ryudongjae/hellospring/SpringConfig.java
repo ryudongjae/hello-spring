5e7e4b0025820fec4c32bbad962347dd675fc29f
ryudongjae <rdj10149@gmail.com>
2021-03-22 03:12:56 +0900

    Commit Update

--- src/main/java/ryudongjae/hellospring/SpringConfig.java
+++ src/main/java/ryudongjae/hellospring/SpringConfig.java
@@ -2,21 +2,32 @@ package ryudongjae.hellospring;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import ryudongjae.hellospring.repository.JdbcMemberRepository;
 import ryudongjae.hellospring.repository.MemberRepository;
-import ryudongjae.hellospring.repository.MemoryMemberRepository;
 import ryudongjae.hellospring.service.MemberService;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
     //Springbean에 등록된다 @Bean
+    private DataSource dataSource;
+
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
 
     @Bean
     public MemberService memberService(){
+
         return new MemberService(memberRepository());
     }
 
     @Bean
     public MemberRepository memberRepository(){
-        return new MemoryMemberRepository();
+
+        //return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
